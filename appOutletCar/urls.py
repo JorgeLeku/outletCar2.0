5ef5267a5d2d06780b6675f6180ef5463d4a7667
Jorge Lekuona <jorge.lekuona@opendeusto.es>
2019-11-24 21:31:49 +0100

    filtros funcionan

--- appOutletCar/urls.py
+++ appOutletCar/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
-    path('listaCochesN', views.listaCochesN, name='listaCochesN'),
+    path('listaCochesN', views.listaCochesNpr.as_view(), name='listaCochesN'),
+    #path('listaCochesN', views.listaCochesN, name='listaCochesN'),
     ]
--- appOutletCar/views.py
+++ appOutletCar/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .models import Coche, FotoCoche, Marca, Modelo, Lugar
 from django.shortcuts import get_object_or_404, get_list_or_404
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView, DetailView
+from .filters import FiltroCoches
 # Create your views here.
 # Devuelve el listado de posts
 class HomePageView(TemplateView):
@@ -17,3 +19,11 @@ def listaCochesN(request):
     coche = get_list_or_404(Coche.objects.order_by('n_bastidor').filter(estado="Segunda mano"))
     context = { 'lista_cochesN': coche }
     return render(request, 'cochesNuevos.html', context)
+
+class listaCochesNpr(ListView):
+    model = Coche
+    template_name = 'cochesNuevos.html'
+    def get_context_data(self, **kwargs):
+        context=super().get_context_data(**kwargs)
+        context['filter']=FiltroCoches(self.request.GET, queryset=self.get_queryset())
+        return context
